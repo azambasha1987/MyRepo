6fdb58910805d1557a58a71dabf15de80fe0f322
Azam Basha <azambasha1987@gmail.com>
2026-09-10 07:52:59 +0530

    feat: add NAPALM BGP automation scripts, configuration files, and Netmiko credential encryption utility

---
+++ PYTHON/2-SSH/c-Napalm/bgp_modified.py
@@ -0,0 +1,13 @@
+
+import json
+from napalm import get_network_driver
+driver = get_network_driver('ios')
+iosv = driver('17.1.1.2', 'david', 'cisco')
+iosv.open()
+ 
+#ios_output = iosv.get_facts()
+#print (json.dumps(ios_output, indent=4)) 
+ 
+ios_output2 = iosv.get_bgp_neighbors()
+print (json.dumps(ios_output2, indent=4))
+

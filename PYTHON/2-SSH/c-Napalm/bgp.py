34b19de28a674ea3dc9ed2c2d34533bd0567e2a1
Azam Basha <azambasha1987@gmail.com>
2026-09-11 16:39:50 +0530

    feat: implement NAPALM automation scripts for BGP neighbor management, OSPF configuration, and audit workflows.

--- PYTHON/2-SSH/c-Napalm/bgp.py
+++ PYTHON/2-SSH/c-Napalm/bgp.py
@@ -2,7 +2,7 @@
 import json
 from napalm import get_network_driver
 driver = get_network_driver('ios')
-iosv = driver('17.1.1.2', 'azam', 'cisco')
+iosv = driver('192.168.1.105', 'azam', 'cisco')
 iosv.open()
  
 #ios_output = iosv.get_facts()
